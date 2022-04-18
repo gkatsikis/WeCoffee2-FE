aa4becf6d6f159bf6579cb92b40bbda699f470a3
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-18 09:30:07 -0400

    add create function in beanservice

--- src/services/beanService.js
+++ src/services/beanService.js
@@ -1,2 +1,19 @@
 import * as tokenService from './tokenService'
 const BASE_URL = `${process.env.REACT_APP_URL}/api/beans`
+
+export const create = async (bean) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(bean)
+    })
+    return await res.json
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
