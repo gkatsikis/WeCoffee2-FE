33c4015ad53da7bd71cb68d69a8fbc9934dc89a0
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-19 08:37:28 -0400

    add input fields

--- src/pages/BeansList/BeansList.jsx
+++ src/pages/BeansList/BeansList.jsx
@@ -12,7 +12,48 @@ const BeansList = (props) => {
     <>
       <h1>What did you try?</h1>
       <form onSubmit={handleSubmit}>
-
+        <label htmlFor="name">Name</label>
+        <input type="text"
+        required
+        name="name"
+        className="bean-input"
+        placeholder=" e.g. La Fortuna"
+        />
+        <label htmlFor="roaster">Roaster</label>
+        <input type="text"
+        required
+        name="roaster"
+        className="bean-input"
+        placeholder=" e.g. Sey Coffee"
+        />
+        <label htmlFor="origin">Origin</label>
+        <input type="text"
+        required
+        name="origin"
+        className="bean-input"
+        placeholder=" e.g. Colombia"
+         />
+         <label htmlFor="region">Region</label>
+         <input type="text"
+         name="region"
+         placeholder=" e.g. Huila"
+          />
+          <label htmlFor="producer">Producer</label>
+          <input type="text"
+          name="producer"
+          placeholder=" e.g. Oscar and Diego Hoyos" 
+          />
+          <label htmlFor="varietals">Varietal(s)</label>
+          <input type="text"
+          name="varietals"
+          placeholder=" e.g. Pink Bourbon"
+           />
+           <label htmlFor="tasting">Tasting Notes</label>
+           <input type="text"
+           required
+           name="tasting"
+           placeholder=" e.g. sweet cherry, tangerine, jasmine" 
+           />
       </form>
     </>
    );
