b5e991cdab8aed055eb179e06d912296d9d2a206
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-24 08:40:19 -0400

    add beancard map function

--- src/components/BeansCard/BeansCard.jsx
+++ src/components/BeansCard/BeansCard.jsx
@@ -1,8 +1,19 @@
 
-const BeansCard = (props) => {
+const BeansCard = ({ beans }) => {
   return ( 
     <>
       <h2>ALL OF THEM</h2>
+      {beans.map(bean => {
+        <>
+        <h3>{bean.name}</h3>
+        <p>{bean.roaster}</p>
+        <p>{bean.origin}</p>
+        <p>{bean.region}</p>
+        <p>{bean.farmer}</p>
+        <p>{bean.varietals}</p>
+        <p>{bean.tasting}</p>
+        </>
+      })}
     </>
    );
 }
