56d38af4bda1151933105a18190dfb2ffae45d06
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-28 14:34:39 -0400

    add beandetails useeffect function

--- src/pages/BeansDetails/BeansDetails.jsx
+++ src/pages/BeansDetails/BeansDetails.jsx
@@ -1,5 +1,20 @@
+import { getOne } from '../../services/beanService'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 
 const BeansDetails = (props) => {
+  const { id } = useParams()
+  const [bean, setBean] = useState()
+
+  useEffect(() => {
+    const fetchOne = async () => {
+      const data = await getOne(id)
+      setBean(data.bean)
+    }
+    fetchOne()
+  }, [id])
+
   return ( 
     <>
       <h1>specific yo</h1>
