b900e303d54ae5bf161a80a6fdc8ca2b08e33c0e
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-16 14:22:00 -0400

    add impression and rating inputs

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,7 +3,16 @@ import './Reviews.css'
 const Reviews = (props) => {
   return ( 
     <>
-      <h1 className="title">Reviews</h1>
+      <label htmlFor="impressions" className="title">Impressions</label>
+      <textarea name="impressions" id="" cols="30" rows="5"></textarea>
+      <label htmlFor="rating" className="title">Rating</label>
+      <select name="rating" className="rating">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
     </>
    );
 }
