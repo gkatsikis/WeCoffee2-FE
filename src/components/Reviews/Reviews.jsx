a91dde99af06c71f7d10fad5ac1e1ba83b9526ea
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-16 14:34:46 -0400

    add handle change function

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,23 +1,37 @@
 import './Reviews.css'
+import React, { useState } from 'react';
+
 
 const Reviews = (props) => {
+  const [form, setForm] = useState({})
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
   return ( 
-    <div className="review">
-      <div className="impressions">
-        <label htmlFor="impressions" className="title">Impressions</label>
-        <textarea name="impressions" cols="30" rows="5"></textarea>
+    <>
+      <div className="review">
+        <form>
+          <div className="impressions">
+            <label htmlFor="impressions" className="title">Impressions</label>
+            <textarea
+            onChange={handleChange} name="impressions" cols="30" rows="5"></textarea>
+          </div>
+          <div className="rating">
+            <label htmlFor="rating" className="title">Rating</label>
+            <select name="rating">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <button type="submit" className='btn btn-primary'>Submit</button>
+        </form>
       </div>
-      <div className="rating">
-        <label htmlFor="rating" className="title">Rating</label>
-        <select name="rating">
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </select>
-      </div>
-    </div>
+    </>
    );
 }
  
