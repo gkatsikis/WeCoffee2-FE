991573bbb125ec1f2068afb3beb8f5eccfa3fc13
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-16 14:29:49 -0400

    add basic styling to review

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,18 +2,22 @@ import './Reviews.css'
 
 const Reviews = (props) => {
   return ( 
-    <>
-      <label htmlFor="impressions" className="title">Impressions</label>
-      <textarea name="impressions" id="" cols="30" rows="5"></textarea>
-      <label htmlFor="rating" className="title">Rating</label>
-      <select name="rating" className="rating">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
-    </>
+    <div className="review">
+      <div className="impressions">
+        <label htmlFor="impressions" className="title">Impressions</label>
+        <textarea name="impressions" cols="30" rows="5"></textarea>
+      </div>
+      <div className="rating">
+        <label htmlFor="rating" className="title">Rating</label>
+        <select name="rating">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+    </div>
    );
 }
  
